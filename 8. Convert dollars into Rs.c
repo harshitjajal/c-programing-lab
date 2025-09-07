53573353697de21c80eb7fd5f25d667293378884
harshitjajal <jajalharshitk123@gmail.com>
2025-09-07 15:52:23 +0530

    Add files via upload

---
+++ 8. Convert dollars into Rs.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+float main()
+{float dollars,Rs;
+ printf("enter value of dollars:\n");
+ scanf("%f",&dollars);
+ Rs=dollars*48;
+ printf("value of Rs is=");
+ printf("%f*48=%f\n",dollars,Rs);
+}
