87d003d6665eff8f39aaeb975482a9a96a1c4752
harshitjajal <jajalharshitk123@gmail.com>
2025-09-07 15:58:11 +0530

    Delete 8. Convert dollars into Rs.c

--- 8. Convert dollars into Rs.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-float main()
-{float dollars,Rs;
- printf("enter value of dollars:\n");
- scanf("%f",&dollars);
- Rs=dollars*48;
- printf("value of Rs is=");
- printf("%f*48=%f\n",dollars,Rs);
-}
