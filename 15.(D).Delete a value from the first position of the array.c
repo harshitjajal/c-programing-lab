d79733ccf7a84fa83ef35515d8947bc4f258ae11
harshitjajal <jajalharshitk123@gmail.com>
2025-10-05 12:24:12 +0530

    Add files via upload

---
+++ 15.(D).Delete a value from the first position of the array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{int a[100],i,num,n,pos=-1;
+ printf("enter the number of how many numbers you add:\n");
+ scanf("%d",&n);
+ if(n<=0||n>100)
+ {printf("task is not possible!\n");
+ return 0;
+ }
+ printf("enter %d values:\n",n);
+ for(i=0;i<n;i++)
+ scanf("%d",&a[i]);
+ if(n==1)
+ {printf("after deletion arrey is not exist!\n");
+ return 0;
+ }
+ printf("arrey after deletion is:\n");
+ for(i=1;i<n;i++)
+ printf("%d\t",a[i]);
+}
