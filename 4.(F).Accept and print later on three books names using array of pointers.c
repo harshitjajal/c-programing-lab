f320c3bf5d33df19b72a249908727f80c883975d
harshitjajal <jajalharshitk123@gmail.com>
2025-10-11 19:45:24 +0530

    Add files via upload

---
+++ 4.(F).Accept and print later on three books names using array of pointers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{int i,j;
+ char *ptr[3],str[3][100];
+ printf("enter name of  your favourite three books\n");
+ for(i=0;i<3;i++)
+ {printf("book %d:",i+1);
+  ptr[i]=str[i];
+  scanf("%s",ptr[i]);
+ }
+ printf("\nname of  your favourite three books are:\n");
+ for(i=0;i<3;i++)
+ {ptr[i]=str[i];
+  printf("book %d:%s\n",i+1,ptr[i]);
+ }
+ return 0;
+}
